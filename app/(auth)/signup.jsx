ca136d4035fac975fba3717aa3b3ea755710c540
Thierry <dev@coworkingcafe.fr>
2025-02-22 17:40:07 +0100

    Mise en place

---
+++ app/(auth)/signup.jsx
@@ -0,0 +1,6 @@
+import { Text } from 'react-native';
+const Signup = () => {
+  return <Text>Signup</Text>;
+};
+
+export default Signup;
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,49 @@
 import '../global.css';
 
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Button, View } from 'react-native';
+import { router } from 'expo-router';
 
-export default function App() {
+const Index = () => {
   return (
     <View className="flex-1 items-center justify-center">
-      <StatusBar style="dark" />
-      <Text className="font-bold">Bienvenue sur ma config with Tailwind !! </Text>
+      <Button
+        title="login"
+        onPress={() => {
+          router.navigate('/auth/login');
+        }}
+      />
+      <Button
+        title="signup"
+        onPress={() => {
+          router.navigate('/auth/signup');
+        }}
+      />
+      <Button
+        title="home"
+        onPress={() => {
+          router.navigate('/main/home/home');
+        }}
+      />
+      <Button
+        title="room"
+        onPress={() => {
+          router.navigate('/main/home/room');
+        }}
+      />
+      <Button
+        title="map"
+        onPress={() => {
+          router.navigate('/main/map');
+        }}
+      />
+      <Button
+        title="profile"
+        onPress={() => {
+          router.navigate('/main/profile');
+        }}
+      />
     </View>
   );
-}
+};
+
+export default Index;
